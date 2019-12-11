517f70031003006b0ca1774bc664911b7be4d246
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-11 17:32:23 -0500

    server uploaded and files loaded

--- server.js
+++ server.js
@@ -4,7 +4,17 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
 
+function logger(req, res, next) {
+	console.log(`${req.method} to ${req.originalUrl} at ${new Date()}`);
+	next();
+}
+
+server.use(logger);
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+server.get('/', (req, res) => {
+	res.send(`<h1>She works!</h2>`);
+});
 
 module.exports = server;
